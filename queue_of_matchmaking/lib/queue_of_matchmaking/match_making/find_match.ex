5d0969c0a826edbb4ac30a4c426695a4943233c6
Todd Pickell <todd@tap-software.com>
2025-10-11 13:39:14 -0500

    start match finding, update mix;

---
+++ queue_of_matchmaking/lib/queue_of_matchmaking/match_making/find_match.ex
@@ -0,0 +1,19 @@
+defmodule QueueOfMatchmaking.MatchMaking do
+  @moduledoc """
+  Entry point for match making functionality
+  """
+
+  @spec find_match([Request]) :: {:ok, [Request]} | {:error, :no_matches}
+  def find_match(queue) when length(queue) < 2 do
+    {:error, :no_matches}
+  end
+
+  def find_match(queue) when length(queue) == 2 do
+    queue
+  end
+
+  def find_match(_queue) do
+    # TODO - implement matching logic passing the queue with probable matches
+    {:error, :no_matches}
+  end
+end
--- queue_of_matchmaking/mix.exs
+++ queue_of_matchmaking/mix.exs
@@ -9,7 +9,20 @@ defmodule QueueOfMatchmaking.MixProject do
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      docs: docs(),
+      dialyzer: [
+        plt_add_apps: [:mix, :ex_unit, :credo],
+        ignore_warnings: ".dialyzer_ignore.exs"
+      ],
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [
+        coveralls: :test,
+        "coveralls.detail": :test,
+        "coveralls.post": :test,
+        "coveralls.html": :test,
+        "coveralls.json": :test
+      ]
     ]
   end
 
@@ -43,7 +56,15 @@ defmodule QueueOfMatchmaking.MixProject do
       {:gettext, "~> 0.26"},
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
-      {:bandit, "~> 1.5"}
+      {:bandit, "~> 1.5"},
+
+      {:credo, "~> 1.7", only: [:dev, :test], runtime: false},
+      {:dialyxir, "~> 1.4", only: [:dev, :test], runtime: false},
+      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
+      {:mix_audit, "~> 2.0", only: [:dev, :test], runtime: false},
+      {:mix_test_watch, "~> 1.1", only: [:dev, :test], runtime: false},
+      {:sobelow, "~> 0.11", only: :dev},
+      {:excoveralls, "~> 0.18", only: :test}
     ]
   end
 
@@ -55,7 +76,61 @@ defmodule QueueOfMatchmaking.MixProject do
   # See the documentation for `Mix` for more info on aliases.
   defp aliases do
     [
-      setup: ["deps.get"]
+      setup: ["deps.get"],
+      mini_ci: [
+        "clean",
+        "compile --all-warnings --warnings-as-errors",
+        "credo",
+        "deps.audit",
+        "deps.unlock --check-unused",
+        "format --check-formatted",
+        "sobelow --config",
+        "dialyzer --format github",
+      ],
+      cov: ["cmd MIX_ENV=test mix coveralls.lcov"]
     ]
   end
+
+  defp docs do
+    [
+      main: "readme",
+      logo: "documentation/logo.png",
+      formatters: ["html"],
+      extras: extras(),
+      before_closing_body_tag: &before_closing_body_tag/1
+    ]
+  end
+
+  defp extras do
+    Path.wildcard("documentation/**/*.{md,livemd}") ++ ["README.md"]
+  end
+
+  defp before_closing_body_tag(:html) do
+    """
+    <script src="https://cdn.jsdelivr.net/npm/mermaid@10.2.3/dist/mermaid.min.js"></script>
+    <script>
+    document.addEventListener("DOMContentLoaded", function () {
+    mermaid.initialize({
+      startOnLoad: false,
+      theme: document.body.className.includes("dark") ? "dark" : "default"
+    });
+    let id = 0;
+    for (const codeEl of document.querySelectorAll("pre code.mermaid")) {
+      const preEl = codeEl.parentElement;
+      const graphDefinition = codeEl.textContent;
+      const graphEl = document.createElement("div");
+      const graphId = "mermaid-graph-" + id++;
+      mermaid.render(graphId, graphDefinition).then(({svg, bindFunctions}) => {
+        graphEl.innerHTML = svg;
+        bindFunctions?.(graphEl);
+        preEl.insertAdjacentElement("afterend", graphEl);
+        preEl.remove();
+      });
+    }
+    });
+    </script>
+    """
+  end
+
+  defp before_closing_body_tag(_), do: ""
 end
